72ef8c9960a07e2a47bedcb26ac271d65c9bce9b
Fabio T Alves <ftalves@poli.ufrj.br>
2021-07-15 03:10:40 -0300

    Scraping link URLs
    Also scraping a single video URL as reference for future access loop

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ require("dotenv").config({ encoding: "utf8", debug: true });
 
 console.log(process.env.PASSWORD.length);
 
+/*
 const BASE_URL = "https://xpcorp.gama.academy/";
 const PLAYLIST_URL = "aluno/playlist/372/2716";
+*/
+
+const BASE_URL = "http://localhost:5500/";
+const PLAYLIST_URL = "index.html";
+
 const resultsPath = "./results/";
 
 (async () => {
@@ -15,6 +21,8 @@ const resultsPath = "./results/";
   const page = await browser.newPage();
   /* Go to the login page and wait for it to load */
   await page.goto(BASE_URL, { waitUntil: "networkidle0" });
+
+  /* WORKING
   await page.screenshot({ path: `${resultsPath}login.png` });
 
   // Login
@@ -26,44 +34,54 @@ const resultsPath = "./results/";
   await page.click('input[type="submit"][name="commit"]');
   console.log("Clicked!");
   await page.screenshot({ path: `${resultsPath}logged.png` });
+  
+  
+  WORKING */
 
   // Go to playlist page and wait for it to load
   console.log("Navigating to playlist");
   await page.goto(BASE_URL + PLAYLIST_URL, { waitUntil: "networkidle0" });
   await page.screenshot({ path: `${resultsPath}playlist.png` });
 
-  /* Run javascript inside of the page */
+  // Run javascript inside of the page
   let data = await page.evaluate(() => {
     let list = document.getElementsByClassName("sidebar-brand toggled")[0];
 
     // Get list of links
-    //let links = list.getElementsByTagName("A");
-    let links = document.links;
-    console.log(links);
-
-    /*
-    for (let link in links) {
-        console.log(link.href());
+    let links = Array.from(list.querySelectorAll("a")).map((anchor) => ({
+      url: anchor.href,
+      title: anchor.textContent,
     }
-    */
-
-    // Construct result object
-    /*
-    links.map((link, i) => {
-      const linkURL = link.href();
-      console.log(linkURL);
-    });
-    */
+    ));
 
     /* Returning an object filled with the scraped data */
     return {
       links,
     };
   });
-  /* Outputting what we scraped */
-  console.log(data.links);
-  for (let i = 0; i < data.links.length; i++) {
-    console.log(data.links.item(i));
-  }
+  // Outputting links list
+  //console.log(data.links);
+  const size = Object.keys(data.links).length;
+  console.log(size);
+  await Promise.all(data.links.map(async (pair, i) => {
+      console.log(pair.url);
+  }))
+
+  
+
+  // Extracting video URL
+  let videoURL = await page.evaluate(() => {
+    let iframeList = Array.from(document.getElementsByTagName("iframe")).map(
+      (anchor) => anchor.src
+    );
+
+    return iframeList[0];
+  });
+
+  console.log(videoURL);
+
+  //data = JSON.stringify(data, null, 2);
+  //fs.writeFileSync(resultsPath + "links.json", data);
+
   await browser.close();
 })();
